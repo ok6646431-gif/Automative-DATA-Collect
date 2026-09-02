afb3218d69934e4ced1a8f9234917c4ba15c136c
ok6646431-gif <ok6646431@gmail.com>
2026-09-02 22:18:26 +0900

    Route G0 through hardened DART resolver

---
+++ orchestrator/zero_touch_runner.py
@@ -0,0 +1,9 @@
+"""Runtime entry point that wires the version-tolerant public DART adapter into G0."""
+
+from orchestrator import dart_public_resolver
+from orchestrator import zero_touch_discovery
+
+zero_touch_discovery.discover_dart_keys = dart_public_resolver.discover_dart_keys
+
+if __name__ == "__main__":
+    raise SystemExit(zero_touch_discovery.main())
